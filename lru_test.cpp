eef7e8592475044a4ee0ccca2c85bc470cb36b2f
phzhu <phzhu99@gmail.com>
2023-11-11 19:00:51 -0500

    read csv

--- lru_test.cpp
+++ lru_test.cpp
@@ -1,4 +1,7 @@
+#include <fstream>
 #include <gflags/gflags.h>
+#include <sstream>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -11,22 +14,76 @@ DEFINE_int64(storage_size, 10000, "");
 DEFINE_int64(iterations, 10000, "");
 DEFINE_int64(cache_size, 1000, "");
 DEFINE_double(alpha, 0.2, "");
+DEFINE_string(file, "hm_0.csv", "");
 
 using key_type = std::string;
 using value_type = std::string;
 
-std::unordered_map<key_type, value_type> storage;
-std::vector<key_type> test_keys;
+enum class VisitType {
+  READ = 0,
+  WRITE = 1,
+};
+
+struct Row {
+  long timestamp; // 100ns
+  VisitType type;
+  std::string offset;
+  long length;
+
+  Row() = delete;
+  Row(long ts, VisitType t, const std::string &o, long l)
+      : timestamp(ts), type(t), offset(o), length(l) {}
+  void debug() const {
+    printf("%ld,%d,%s,%ld\n", timestamp, type, offset.c_str(), length);
+  }
+};
+
+std::unordered_map<std::string /* offset */, Row> storage;
 
 void init_storage() {
-  std::srand(std::time(0));
-  for (int i = 0; i < FLAGS_storage_size; ++i) {
-    std::string key = generateRandomString();
-    std::string value = generateRandomString();
-    storage[key] = value;
-    test_keys.emplace_back(key);
+  std::ifstream file(FLAGS_file);
+  if (!file.is_open()) {
+    exit(-2);
   }
-  printf("generated storage, size:%ld\n", FLAGS_storage_size);
+  std::string line;
+
+  std::vector<Row> rows;
+  rows.reserve(4000000);
+
+  long begin_ts = -1;
+
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string value;
+    std::vector<std::string> values;
+    while (std::getline(iss, value, ',')) {
+      values.push_back(value);
+    }
+
+    const std::string &ts = values[0], type = values[3], offset = values[4],
+                      length = values[5];
+    long relative_ts = 0;
+
+    if (begin_ts != -1) {
+      relative_ts = std::stol(ts) - begin_ts;
+    } else {
+      begin_ts = std::stol(ts);
+    }
+    rows.emplace_back(Row(relative_ts,
+                          type == "Read" ? VisitType::READ : VisitType::WRITE,
+                          offset, std::stol(length)));
+  }
+
+  int count = 0;
+  for (const Row &row : rows) {
+    row.debug();
+    ++count;
+    if (count > 10) {
+      break;
+    }
+  }
+
+  printf("generated storage, size:%ld\n", rows.size());
 }
 
 std::pair<size_t, long long> test_basic_lru() {
